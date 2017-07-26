61a9207c942884f93b33ac681a499cdc04a6c164
mbehringer1 <mbknightgb@outlook.com>
2017-07-26 00:02:14 -0500

    Added connectivity tests.  I will remove the api call once I create a service.

--- checkit-webui/src/app/components/app.component.ts
+++ checkit-webui/src/app/components/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: '../templates/app.component.html',
   styleUrls: ['../styles/app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CheckIT';
   fullName = 'Random Dude';
   street = '111 The Blvd.';
@@ -16,4 +17,23 @@ export class AppComponent {
   checkNumber = '1000';
   date = new Date().toLocaleDateString();
   esignature = 'Click here to sign';
+
+  timestamp = '9999-99-99 99:99:99';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  // connectivity tests
+  ngOnInit(): void {
+    this.http.get('http://localhost:9001', { responseType: 'text' }).subscribe(data => {
+      console.log(data);
+      this.timestamp = data;
+    });
+
+    this.http.get('http://localhost:9001/api/NumberToString/Money?value=12345678.90').subscribe(response => {
+      console.log(response);
+      // alert(response);
+    });
+  }
 }
